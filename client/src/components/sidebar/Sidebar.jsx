ed41c02fba87ec48464e75a1674e68e89c18f9d8
Josué Rubio <josu_mr@hotmail.com>
2022-02-07 17:14:11 +0100

    ready for first deployment

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -66,36 +66,36 @@ export default function Sidebar() {
       <div className='sidebarWrapper'>
         <ul className='sidebarList'>
           <li className='sidebarListItem'>
+            <Bookmark className='sidebarIcon' />
+            <span className='sidebarListItemText'>Blog</span>
+          </li>
+          <li className='sidebarListItem'>
             <RssFeed className='sidebarIcon' />
-            <span className='sidebarListItemText'>Feed</span>
+            <span className='sidebarListItemText'>Podcasts</span>
           </li>
           <li className='sidebarListItem'>
             <PlayCircleFilledOutlined className='sidebarIcon' />
             <span className='sidebarListItemText'>Videos</span>
           </li>
           <li className='sidebarListItem'>
-            <Group className='sidebarIcon' />
-            <span className='sidebarListItemText'>Groups</span>
-          </li>
-          <li className='sidebarListItem'>
-            <Bookmark className='sidebarIcon' />
-            <span className='sidebarListItemText'>Bookmarks</span>
-          </li>
-          <li className='sidebarListItem'>
-            <HelpOutline className='sidebarIcon' />
-            <span className='sidebarListItemText'>Questions</span>
-          </li>
-          <li className='sidebarListItem'>
             <WorkOutline className='sidebarIcon' />
-            <span className='sidebarListItemText'>Jobs</span>
-          </li>
-          <li className='sidebarListItem'>
-            <Event className='sidebarIcon' />
-            <span className='sidebarListItemText'>Events</span>
+            <span className='sidebarListItemText'>Crafts</span>
           </li>
           <li className='sidebarListItem'>
             <School className='sidebarIcon' />
-            <span className='sidebarListItemText'>Courses</span>
+            <span className='sidebarListItemText'>One-Shots</span>
+          </li>
+          <li className='sidebarListItem'>
+            <Group className='sidebarIcon' />
+            <span className='sidebarListItemText'>Personajes</span>
+          </li>
+          <li className='sidebarListItem'>
+            <HelpOutline className='sidebarIcon' />
+            <span className='sidebarListItemText'>Sugerencias</span>
+          </li>
+          <li className='sidebarListItem'>
+            <Event className='sidebarIcon' />
+            <span className='sidebarListItemText'>Eventos</span>
           </li>
         </ul>
         <button className='sidebarButton'>Show more</button>
