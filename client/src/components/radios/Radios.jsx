d445ee89febcf49cd48b5b2e5e6dea7956be30ad
Josué Rubio <josu_mr@hotmail.com>
2022-02-14 17:41:56 +0100

    testing heroku

--- client/src/components/radios/Radios.jsx
+++ client/src/components/radios/Radios.jsx
@@ -3,27 +3,27 @@ import './radios.css';
 export default function Radios() {
   return (
     <div className='radios'>
-      <h1 className='podcast Title'>Nuestros podcast preferidos</h1>
-      <div className='spotifyRep'>
+      <h1 className='podcastTitle'>Nuestros audios preferidos</h1>
+      <div className='spotifyWrapper'>
         <iframe
-          className='spotifyBotched'
+          className='spotifyShare'
           src='https://open.spotify.com/embed/show/7eehQh4aqzp64lGHU2ga1G?utm_source=generator&theme=0'
           allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         />
         <iframe
-          className='spotifyBotched'
-          src='https://open.spotify.com/embed/show/7eehQh4aqzp64lGHU2ga1G?utm_source=generator&theme=0'
+          className='spotifyShare'
+          src='https://open.spotify.com/embed/show/7e8zPFBpW0DtgyrrPnt0xT?utm_source=generator&theme=0'
           allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         />
         <iframe
-          className='spotifyBotched'
-          src='https://open.spotify.com/embed/show/7eehQh4aqzp64lGHU2ga1G?utm_source=generator&theme=0'
+          className='spotifyShare'
+          src='https://open.spotify.com/embed/show/04KgPyGwvYx0IfYoqauNxs?utm_source=generator&theme=0'
           allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         />
         <iframe
-          className='spotifyBotched'
-          src='https://open.spotify.com/embed/show/7eehQh4aqzp64lGHU2ga1G?utm_source=generator&theme=0'
+          src='https://open.spotify.com/embed/playlist/1EbvxtnfOB17TiK50pI6mH?utm_source=generator'
           allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+          className='spotifyPlaylist'
         />
       </div>
     </div>
--- index.js
+++ index.js
@@ -25,6 +25,17 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Middleware
